c6dedb532f986973e31e7c906168275597efd812
pardhavsai <pardhav0201@gmail.com>
2026-09-13 12:28:30 +0530

    Add cart item query method

--- src/main/java/com/pardhavsai/ecommerce/repository/CartItemRepository.java
+++ src/main/java/com/pardhavsai/ecommerce/repository/CartItemRepository.java
@@ -1,5 +1,11 @@
 package com.pardhavsai.ecommerce.repository;
+
 import com.pardhavsai.ecommerce.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.Optional;
-public interface CartItemRepository extends JpaRepository<CartItem, Long> { Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId); }
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
+    List<CartItem> findByCartId(Long cartId);
+}
