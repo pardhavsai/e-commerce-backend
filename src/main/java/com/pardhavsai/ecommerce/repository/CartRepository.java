a3a793dc00d09d1d15c2b778e11a48a0a2775a03
pardhavsai <pardhav0201@gmail.com>
2026-09-13 12:24:24 +0530

    Add cart repository

---
+++ src/main/java/com/pardhavsai/ecommerce/repository/CartRepository.java
@@ -0,0 +1,5 @@
+package com.pardhavsai.ecommerce.repository;
+import com.pardhavsai.ecommerce.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface CartRepository extends JpaRepository<Cart, Long> { Optional<Cart> findByUserId(Long userId); }
